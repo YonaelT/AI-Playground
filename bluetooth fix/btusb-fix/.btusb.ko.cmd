1bb0f5cdf8e792f55c14237f67fcaeea89391384
Yonael <162355073+YonaelT@users.noreply.github.com>
2026-08-14 07:28:27 +0300

    bluetooth init

---
+++ bluetooth fix/btusb-fix/.btusb.ko.cmd
@@ -0,0 +1,1 @@
+savedcmd_btusb.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T /usr/lib/modules/7.1.5-arch1-2/build/scripts/module.lds -o btusb.ko btusb.o btusb.mod.o .module-common.o
---
+++ bluetooth fix/btusb-fix/.modules.order.cmd
@@ -0,0 +1,1 @@
+savedcmd_modules.order := {   echo btusb.o; :; } > modules.order
